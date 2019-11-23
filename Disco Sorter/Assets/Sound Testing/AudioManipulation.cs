fcc7b6d5b7514174c8d8011d9ab5910adfccaf0c
MikeMG <michalek.mg@gmail.com>
2019-11-24 00:36:33 +0100

    BeatEditor

---
+++ Disco Sorter/Assets/Sound Testing/AudioManipulation.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManipulation : MonoBehaviour
+{
+    public float time;
+    AudioSource a;
+    bool pausePressed = false;
+
+    void Start()
+    {
+        a = GetComponent<AudioSource>();
+        a.time = time;
+        a.Play();
+    }
+
+    public void PausePlay()
+    {
+        if (pausePressed == false)
+        {
+            GetComponent<SongController>().enabled = false;
+            time = a.time;
+            pausePressed = !pausePressed;
+            a.Stop();
+        }
+        else
+        {
+            GetComponent<SongController>().enabled = true;
+            a.time = time;
+            pausePressed = !pausePressed;
+            a.Play();
+        }
+
+
+    }
+}
