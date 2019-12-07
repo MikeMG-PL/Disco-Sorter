184020018bf4737833ef49c50ae2c100f8facaa9
MikeMG <michalek.mg@gmail.com>
2019-12-07 02:00:25 +0100

    editor

--- Disco Sorter/Assets/Sound Testing/AudioManipulation.cs
+++ Disco Sorter/Assets/Sound Testing/AudioManipulation.cs
@@ -1,37 +1,84 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
 public class AudioManipulation : MonoBehaviour
 {
-    public float time;
+    float time;
+    float clampedLength;
     AudioSource a;
     bool pausePressed = false;
+    public Slider slider;
 
     void Start()
     {
         a = GetComponent<AudioSource>();
         a.time = time;
-        a.Play();
+        pausePressed = true;
     }
 
-    public void PausePlay()
+    public void Pause()
     {
         if (pausePressed == false)
         {
             GetComponent<SongController>().enabled = false;
             time = a.time;
-            pausePressed = !pausePressed;
+            pausePressed = true;
             a.Stop();
         }
-        else
+    }
+
+    public void Play()
+    {
+        if (pausePressed == true)
         {
             GetComponent<SongController>().enabled = true;
             a.time = time;
-            pausePressed = !pausePressed;
+            pausePressed = false;
             a.Play();
         }
+    }
 
+    void Clamp()
+    {
+        if (a.isPlaying)
+        {
+            clampedLength = a.time / a.clip.length;
+            Debug.Log(clampedLength);
+        }
+    }
 
+    void OnClipEnd()
+    {
+        if (!a.isPlaying && a.time > 0.1f)
+            Pause();
+    }
+
+    public void Test()
+    {
+        Debug.Log("Klik!");
+    }
+
+    void Slider()
+    {
+        if (a.isPlaying)
+        {
+            slider.value = clampedLength;
+        }
+    }
+
+    public void OnSliderRelease()
+    {
+        Debug.Log("Slider released!");
+        time = slider.value * a.clip.length;
+    }
+
+    void Update()
+    {
+        Clamp();
+        Slider();
+        OnClipEnd();
     }
 }
