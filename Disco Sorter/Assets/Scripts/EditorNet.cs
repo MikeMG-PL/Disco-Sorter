e026d939c4e14f360ab1bb8daf1abfad7b0850a7
Mikołaj Przybylski <clovers02123@gmail.com>
2019-12-12 18:36:25 +0100

    Zalążki skryptu siatki

---
+++ Disco Sorter/Assets/Scripts/EditorNet.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using UnityEngine;
+
+public class EditorNet : MonoBehaviour
+{
+    public GameObject entity;
+    public AudioClip clip;
+    //public Material activeMaterial;
+
+    private Vector3 positionToSpawnEntity = Vector3.zero;
+    private GameObject createdEntity;
+    private GameObject[] entityArray;
+
+    void Start()
+    {
+        entityArray = new GameObject[Convert.ToInt32(clip.length)];
+        for (int i = 0; i < clip.length; i++)
+        {
+            createdEntity = Instantiate(entity, positionToSpawnEntity, Quaternion.identity);
+            createdEntity.GetComponent<Entity>().EntityNumber = i;
+            entityArray[i] = createdEntity;
+            positionToSpawnEntity.x = positionToSpawnEntity.x + 1.05f;
+        }
+    }
+
+    private double currentTime;
+    private int entityNumber;
+    void Update()
+    {
+        currentTime = gameObject.GetComponent<AudioSource>().time;
+        entityNumber = Convert.ToInt32(currentTime);
+        if (entityArray[0] != null)
+            entityArray[entityNumber].GetComponent<Renderer>().material.color = Color.green;
+    }
+}
---
+++ Disco Sorter/Assets/Scripts/Entity.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Entity : MonoBehaviour
+{
+    public int EntityNumber;
+    public enum EntityType { Slap, Smash, DontTouch };
+    public EntityType entityType;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
