9cded08b812668a5264c99e653fc35ddf5bd9883
MikeMG <michalek.mg@gmail.com>
2020-01-15 10:52:30 +0100

    Marker Follow Switch
    dodano przełącznik który pozwala designerowi zdecydować czy kamera ma podążać za aktualnym momentem czy też nie

--- Disco Sorter/Assets/Scripts/EditorScripts/EditorCamera.cs
+++ Disco Sorter/Assets/Scripts/EditorScripts/EditorCamera.cs
@@ -5,14 +5,22 @@ public class EditorCamera : MonoBehaviour
     [SerializeField]
     private float cameraSpeed = 3.5f;
     public GameObject songController;
+    public bool followMarkerSwitch;
     private float scrollInput;
     private Vector3 pos;
     private Vector3 newPos;
 
     void Update()
     {
-        CameraMove();
-        FollowMarker();
+        if (followMarkerSwitch) // Jeśli gracz odznaczył podążanie kamery za znacznikiem
+            CameraMove();
+        else
+            FollowMarker(); // Jeśli gracz zaznaczył podążanie kamery za znacznikiem
+    }
+
+    public void SwitchBool()
+    {
+        followMarkerSwitch = !followMarkerSwitch;
     }
 
     // Odpowiada za ruch kamery za pomocą scrolla
@@ -29,10 +37,14 @@ public class EditorCamera : MonoBehaviour
 
     public void FollowMarker()
     {
-        if (songController.GetComponent<EntityCurrentTimeHighlight>().currentEntity != null)
-            pos = songController.GetComponent<EntityCurrentTimeHighlight>().currentEntity.transform.position;
-        newPos = new Vector3(pos.x, gameObject.transform.GetChild(0).position.y, pos.z);
+        GameObject currentEntity = songController.GetComponent<EntityCurrentTimeHighlight>().currentEntity;
+        Transform child = gameObject.transform.GetChild(0);
 
-        gameObject.transform.GetChild(0).position = Vector3.Lerp(gameObject.transform.GetChild(0).position, newPos, 0.1f);
+        if (currentEntity != null)
+            pos = currentEntity.transform.position;
+
+        newPos = new Vector3(pos.x, child.position.y, pos.z);
+
+        child.position = Vector3.Lerp(child.position, newPos, 0.1f);
     }
 }
