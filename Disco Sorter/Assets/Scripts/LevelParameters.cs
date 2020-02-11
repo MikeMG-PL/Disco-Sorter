5f7115f95e01bb70d91bd56a6804f807421b8d67
MikeMG <michalek.mg@gmail.com>
2020-02-11 18:55:02 +0100

    OnScreen Panel

---
+++ Disco Sorter/Assets/Scripts/LevelParameters.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelParameters : MonoBehaviour
+{
+    public List<int> entityType = new List<int>();
+    public List<int> color = new List<int>();
+    public List<int> action = new List<int>();
+    public List<int> linkedReleaseEN = new List<int>();
+    public List<int> linkedCatchEN = new List<int>();
+    public int BPM, netDensity;
+}
---
+++ Disco Sorter/Assets/Scripts/LoadToScene.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LoadToScene : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject savesPanel, entityMenu;       // Panel, w którym znajdują się przyciski do wyboru pliku, do wczytania. Obiekt zawierający skrypt entityMenu
+    private EditorNet editorNet;
+
+    private void Awake()
+    {
+        editorNet = GetComponent<EditorNet>();
+    }
+
+    public void SaveSong(bool forceSave)
+    {
+        entityMenu.GetComponent<EntityMenu>().IsSavedChange(true);
+        SongFile.SaveSong(gameObject.GetComponent<EditorNet>());
+    }
+
+    // Wczytuje dany plik, na podstawie int przekazanego przez przycisk znajdujący się w savesPanel
+    public void LoadSong(int selectedButton)
+    {
+        savesPanel.SetActive(false);
+        string[] songNames = SongFile.GetSavesNames();
+
+        if (selectedButton >= songNames.Length)
+            return;
+
+        SongData songData = SongFile.LoadSong(songNames[selectedButton]);
+
+        editorNet.BPM = songData.BPM;
+        editorNet.netDensity = songData.netDensity;
+        editorNet.BuildNet();
+
+        for (int i = 0; i < editorNet.entityArray.Length; i++)
+        {
+            Entity entity = editorNet.entityArray[i].GetComponent<Entity>();
+            entity.type = songData.entityType[i];
+            entity.color = songData.color[i];
+            entity.action = songData.action[i];
+            entity.ChangeColor();
+            entity.ChangeTypeIcon();
+            entity.ChangeActionIcon();
+        }
+    }
+}
