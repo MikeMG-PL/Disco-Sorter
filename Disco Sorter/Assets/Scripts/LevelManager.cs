2e6837538b03201bdc229ac0df3209ac8b688973
MikeMG <michalek.mg@gmail.com>
2020-04-04 23:22:50 +0200

    GameObjects finally SYNCED with music!

--- Disco Sorter/Assets/Scripts/LevelManager.cs
+++ Disco Sorter/Assets/Scripts/LevelManager.cs
@@ -5,59 +5,60 @@ using UnityEngine;
 public class LevelManager : MonoBehaviour
 {
     [HideInInspector()]
-    public int levelIndex = 0;
+    public int levelIndex;
     [HideInInspector()]
     public List<string> LevelList = new List<string>();
     public AudioManipulation songController;
     LevelParameters level;
-    int iterator = 0, iterator2 = 0;
+    int iterator;
 
 
-    float spawnTime, timer; bool timerStarted = false;
+    float spawnTime, timer; bool timerStarted;
 
     Vector3 A, B, C;
     public Transform TransformOfB;
     public GameObject testBallPrefab;
 
-    public Queue<GameObject> spawnPipeline = new Queue<GameObject>();
+    public List<GameObject> spawnPipeline = new List<GameObject>();
 
-
-    void LoadLevel()
+    void Start()
     {
         level = GetComponent<LevelParameters>();
+        GetComponent<LoadToScene>().LoadSong(levelIndex);
+        spawnPipeline = level.spawnPipeline;
+        Calculations();
 
-        if (timerStarted && iterator == 0)
+        for(int i = 0; i < spawnPipeline.Count; i++)
         {
-            GetComponent<LoadToScene>().LoadSong(levelIndex);
-            SetPipeline();
-            iterator++;
+            Debug.Log(spawnPipeline[i].GetComponent<ObjectParameters>().spawnTime);
         }
-        else if (timerStarted)
-        {
-            timer += Time.deltaTime;
-            PlayMusic();
-            SpawnObjects();
-        }
-        if (Input.GetKeyDown(KeyCode.Return) && levelIndex < LevelList.Count - 1)
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return) && levelIndex < LevelList.Count - 1 && !songController.a.isPlaying)
         {
             timerStarted = true;
-            Calculations();
-            //Instantiate(testBallPrefab, C, Quaternion.identity);
+        }
+
+        if (timerStarted)
+        {
+            PlayMusic();
+            SpawnObjects();
+            timer += Time.deltaTime;
         }
     }
 
     void Calculations()
     {
-        var b = (Mathf.Pow(level.rollTime, 2) * Mathf.Abs(Physics.gravity.y) * Mathf.Sin(0.174532925f)) / (2 * (Mathf.Pow(Mathf.Sin(0.174532925f), 2) + 1) * Mathf.Sqrt(1 - Mathf.Pow(Mathf.Sin(0.174532925f), 2)));
+        var b = (Mathf.Pow(level.rollTime, 2) * Mathf.Abs(Physics.gravity.y) * Mathf.Sin(0.174532925f)) /
+                (2 * (Mathf.Pow(Mathf.Sin(0.174532925f), 2) + 1) * Mathf.Sqrt(1 - Mathf.Pow(Mathf.Sin(0.174532925f), 2)));
+
         var h = (b * Mathf.Sin(0.174532925f)) / Mathf.Sqrt(1 - Mathf.Pow(Mathf.Sin(0.174532925f), 2));
 
         B = TransformOfB.position;
         A = new Vector3 { x = B.x, y = B.y, z = B.z + b };
         C = new Vector3 { x = A.x, y = A.y + h, z = A.z };
-    }
-
-    void Start()
-    {
     }
 
     void PlayMusic()
@@ -66,51 +67,17 @@ public class LevelManager : MonoBehaviour
             songController.a.Play();
     }
 
-    void SetPipeline()
-    {
-        for (int i = 0; i < level.spawnPipeline.Count; i++)
-        {
-            spawnPipeline.Enqueue(level.spawnPipeline[i]);
-        }
-    }
-
     void SpawnObjects()
     {
-        if (songController.a.isPlaying && iterator2 < level.spawnPipeline.Count)
-            spawnTime = level.spawnPipeline[iterator2].GetComponent<ObjectParameters>().spawnTime;
-
-        if (timer >= spawnTime - 0.01f && timer <= spawnTime + 0.01f && timerStarted)
+        if (iterator < spawnPipeline.Count)
         {
-            Instantiate(testBallPrefab, C, Quaternion.identity); // 
-            //spawnPipeline.Dequeue();
-            iterator2++;
+            spawnTime = spawnPipeline[iterator].GetComponent<ObjectParameters>().spawnTime;
+
+            if (timer >= spawnTime - 0.01f && timer <= spawnTime + 0.01f && timerStarted)
+            {
+                Instantiate(testBallPrefab, C, Quaternion.identity);
+                iterator++;
+            }
         }
-        Debug.Log(timer + " | SPAWNTIME: " +  spawnTime);
-    }
-
-    void RythmCheck()
-    {
-
-    }
-
-    void Update()
-    {
-        LoadLevel();
-        //Debug.Log(timer);
-        // if (timerStarted)
-        {
-            //timer += Time.deltaTime; testing
-            //PlayMusic();
-        }
-
-        //if (Input.GetKeyDown(KeyCode.Return)) // START
-        {
-            //  timerStarted = true;
-            //  SpawnObjects();
-            //  Instantiate(testBallPrefab, C, Quaternion.identity); // testing
-        }
-
-
-
     }
 }
