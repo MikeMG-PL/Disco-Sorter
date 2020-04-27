d143dba51693aeff596c7ba1234fae60595ad076
MikeMG <michalek.mg@gmail.com>
2020-04-27 11:23:24 +0200

    basics of SO level saving

---
+++ Disco Sorter/Assets/Prefabs/Levels/ScriptableObjectFactory.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScriptableObjectFactory : MonoBehaviour
+{
+    public Level levelTemplate; // BAZOWY SCRIPTABLE OBJECT POZIOMU
+
+
+}
