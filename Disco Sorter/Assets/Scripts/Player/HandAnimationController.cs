438fe4cf9034f3b180d6faf4adf69c2c1569d810
Purban <xpurbanx@wp.pl>
2020-05-03 23:14:56 +0200

    WIP picking up dropzones

---
+++ Disco Sorter/Assets/Scripts/Player/HandAnimationController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+public class HandAnimationController : MonoBehaviour
+{
+    public float handFistStage;
+    public VRTK_ControllerEvents controllerEvents;
+    public Animator handAnimator;
+
+    private void Start()
+    {
+        controllerEvents = GetComponent<VRTK_ControllerEvents>();
+    }
+
+    private void Update()
+    {
+        //trigger axis doesn't work somehow, need to check on Quest
+        handFistStage = controllerEvents.GetTriggerAxis();
+        //handFistStage = controllerEvents.GetGripAxis();
+        //controllerEvents.GetGripAxis();
+        //handFistStage = 0.9f;
+        handAnimator.SetFloat("TriggerPressed", handFistStage);
+
+    }
+}
