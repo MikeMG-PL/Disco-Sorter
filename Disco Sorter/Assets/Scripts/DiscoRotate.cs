621817ba713903154ab033048d0abe88dd13d0e0
MikeMG <michalek.mg@gmail.com>
2020-05-27 13:29:54 +0200

    lighting and new cubemaps for disco

---
+++ Disco Sorter/Assets/Scripts/DiscoRotate.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DiscoRotate : MonoBehaviour
+{
+    public float speed = 10;
+
+    void Start()
+    {
+        if (speed > 360)
+            speed = 360;
+    }
+
+    void Update()
+    {
+        transform.localEulerAngles += new Vector3(0, speed * Time.deltaTime, 0);
+        if(transform.localEulerAngles.y >= 360)
+            transform.localEulerAngles -= new Vector3(0, 360 * Time.deltaTime, 0);
+    }
+}
