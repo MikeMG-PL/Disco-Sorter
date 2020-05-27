ab1cf3651d00f4619ba3b132ce6d61d00e5d981f
MikeMG <michalek.mg@gmail.com>
2020-05-27 20:25:31 +0200

    Shader, GREAT LIGHTS, fake bloom

---
+++ Disco Sorter/Assets/Materials/Bloom/AutoColor.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoColor : MonoBehaviour
+{
+    public enum LampColor { blue, green, pink, red, white, yellow };
+
+    public LampColor color;
+    bool thisIsMaluch, thisIsLightbar;
+
+    public List<Material> materials = new List<Material>();
+    public List<Material> fogs = new List<Material>();
+
+    void Start()
+    {
+        switch(gameObject.tag)
+        {
+            case "Lightbar":
+                thisIsLightbar = true;
+                thisIsMaluch = false;
+                break;
+            case "Maluch":
+                thisIsLightbar = false;
+                thisIsMaluch = true;
+                break;
+            default:
+                thisIsLightbar = false;
+                thisIsMaluch = false;
+                Debug.LogError("There is an object in the scene which has neither \"Maluch\" nor \"Lightbar\" tag.");
+                break;
+        }
+    }
+
+    void Update()
+    {
+        if (thisIsMaluch)
+            SetColorMaluch(color);
+        else
+            SetColorLightbar(color);
+    }
+
+    void SetColorMaluch(LampColor c)
+    {
+        transform.GetChild(2).GetComponent<MeshRenderer>().material = materials[(int)color];
+        transform.GetChild(2).GetChild(0).GetComponent<MeshRenderer>().material = fogs[(int)color];
+        transform.GetChild(2).GetChild(1).GetComponent<MeshRenderer>().material = materials[(int)color];
+    }
+
+    void SetColorLightbar(LampColor c)
+    {
+        transform.GetChild(0).GetChild(1).GetComponent<MeshRenderer>().material = materials[(int)color];
+        transform.GetChild(0).GetChild(2).GetComponent<MeshRenderer>().material = materials[(int)color];
+        transform.GetChild(0).GetChild(3).GetComponent<MeshRenderer>().material = materials[(int)color];
+        transform.GetChild(0).GetChild(4).GetComponent<MeshRenderer>().material = materials[(int)color];
+        transform.GetChild(0).GetChild(5).GetComponent<MeshRenderer>().material = materials[(int)color];
+        transform.GetChild(0).GetChild(6).GetComponent<MeshRenderer>().material = materials[(int)color];
+    }
+
+}
+
+// Material.setColor(„_Color”, new Color())
---
+++ Disco Sorter/Assets/Shaders/UnlitFakeVolumeFog.shader
@@ -0,0 +1,71 @@
+﻿Shader "Unlit/Fake Volume Fog"
+{
+	Properties
+	{
+		_MainTex("Gradient", 2D) = "white" {}
+		_Tint("Color", Color) = (1,1,1,1)
+		_AlphaPower("Alpha Power", Float) = 1
+		[KeywordEnum(Off, Front, Back)] _CullMode("Double Sided", Int) = 2
+	}
+
+		SubShader
+		{
+			Tags {"Queue" = "Transparent" "IgnoreProjector" = "True" "RenderType" = "Transparent"}
+			LOD 100
+
+			ZWrite Off
+			Blend SrcAlpha OneMinusSrcAlpha
+			Cull [_CullMode]
+
+			Pass 
+			{
+			CGPROGRAM
+				#pragma vertex vert
+				#pragma fragment frag
+				#pragma target 2.0
+				#pragma multi_compile_fog
+
+				#include "UnityCG.cginc"
+
+				struct appdata_t {
+					float4 vertex : POSITION;
+					float2 texcoord : TEXCOORD0;
+					UNITY_VERTEX_INPUT_INSTANCE_ID
+				};
+
+				struct v2f {
+					float4 vertex : SV_POSITION;
+					float2 texcoord : TEXCOORD0;
+					UNITY_FOG_COORDS(1)
+					UNITY_VERTEX_OUTPUT_STEREO
+				};
+
+				sampler2D _MainTex;
+				float4 _MainTex_ST;
+
+				fixed4 _Tint;
+				float _AlphaPower;
+
+				v2f vert(appdata_t v)
+				{
+					v2f o;
+					UNITY_SETUP_INSTANCE_ID(v);
+					UNITY_INITIALIZE_VERTEX_OUTPUT_STEREO(o);
+					o.vertex = UnityObjectToClipPos(v.vertex);
+					o.texcoord = TRANSFORM_TEX(v.texcoord, _MainTex);
+					UNITY_TRANSFER_FOG(o,o.vertex);
+					return o;
+				}
+
+				fixed4 frag(v2f i) : SV_Target
+				{
+					fixed4 col = tex2D(_MainTex, i.texcoord) * _Tint;
+					col.a = pow(col.a, _AlphaPower);
+					UNITY_APPLY_FOG(i.fogCoord, col);
+					return col;
+				}
+			ENDCG
+			}
+	}
+
+}
