1a71bd4f8cbee9101e6ab2d07efcbd543cb423b3
MikeMG <michalek.mg@gmail.com>
2020-06-21 21:16:10 +0200

    Sound settings, another build

--- Disco Sorter/Assets/Scripts/ControllerRendererDisable.cs
+++ Disco Sorter/Assets/Scripts/ControllerRendererDisable.cs
@@ -6,7 +6,7 @@ public class ControllerRendererDisable : MonoBehaviour
 {
     Renderer[] renderers;
 
-    void Start()
+    void Update()
     {
         renderers = GetComponentsInChildren<Renderer>();
 
