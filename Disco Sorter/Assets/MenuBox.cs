d9cb1850d6a3cde2ec047e362b871a5c5297c162
Mikołaj Przybylski <clovers02123@gmail.com>
2020-06-29 21:10:29 +0200

    Testing scene changing, menu

---
+++ Disco Sorter/Assets/MenuBox.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuBox : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.transform.parent == null) return;
+
+        if (other.transform.parent.CompareTag("Apple"))
+        {
+            SceneManager.LoadScene("2.GAME");
+        }
+    }
+}
